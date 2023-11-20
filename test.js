bbb825f77628190e38d131350cbfd033f9170fc5
devnuckles <mohammadroman925@gmail.com>
2023-11-20 16:07:06 +0600

    Convert Class Component to Functional Component

---
+++ test.js
@@ -0,0 +1,18 @@
+/*
+React Lifecycle:
+
+ # react component phases
+
+    a. Mounting phase (জন্ম নেয়)
+      i. constructor method 
+      ii. render method 
+      iii. componentDidMount method
+
+    b. Updating phase (props or state changes)
+      i. render method
+      ii. componentDidUpdate method
+
+    c. Unmounting phase
+      i. componentDidUnmount method
+
+*/
